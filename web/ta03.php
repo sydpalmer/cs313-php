be96606107c5b2e3549e135b023aecda54311f65
sydpalmer <hay14004@byui.edu>
2018-01-23 20:55:37 -0700

    ta03 php done

--- web/ta03.php
+++ web/ta03.php
@@ -4,7 +4,7 @@
 	$major = $_POST["major"];
 	$comments = $_POST["comments"];
 
-	echo "Your name is: " . $name . "<br>Your email is: " . $email . " <a href='mailto:" . $email . "'>" . "Send mail!" . "</a>" . "<br>Your major is: " . $major . "<br>Comments: " . $comments . "<br>";
+	echo "<b>Your name is: </b>" . $name . "<br>Your email is: " . $email . " <a href='mailto:" . $email . "'>" . "Send mail!" . "</a>" . "<br>Your major is: " . $major . "<br>Comments: " . $comments . "<br>";
 	if(!empty($_POST["continent"])){
 		echo "You have visited: <br>";
 		foreach ($_POST["continent"] as $selected) {
