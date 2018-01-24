9f905d7601ddee6c852370ecce789d93ffe402b7
sydpalmer <hay14004@byui.edu>
2018-01-23 21:03:19 -0700

    ta03 php done

--- web/ta03.php
+++ web/ta03.php
@@ -6,9 +6,10 @@
 
 	echo "<b>Your name is: </b>" . $name . "<br><b>Your email is: </b>" . $email . " <a href='mailto:" . $email . "'>" . "Send mail!" . "</a>" . "<br><b>Your major is: </b>" . $major . "<br><b>Comments: </b>" . $comments . "<br>";
 	if(!empty($_POST["continent"])){
-		echo "<b>You have visited: </b><br>";
+		echo "<b>You have visited: </b><br>" . "<ul>";
 		foreach ($_POST["continent"] as $selected) {
-			echo $selected . "<br>";
+			echo "<li>" . $selected . "</li>";
 		}
+		echo "</ul>";
 	}
 ?>
