e7360c8c23cd066e1ef2220a9dd84f8ed748725f
sydpalmer <hay14004@byui.edu>
2018-01-27 11:50:20 -0700

    added some JS

--- web/shoppingCart.js
+++ web/shoppingCart.js
@@ -163,7 +163,7 @@ function loadCart(){
 	len = added.length;
 	text = "<ol>"
 	for(i = 0; i < len; i++){
-		text += "<li>" + added[i] + "</li><br>";
+		text += "<li align='center;'>" + added[i] + "</li><br>";
 	}
 	text += "</ol>"
 
