264c8e2e212870693d99d4713651fd1c2e19282c
sydpalmer <hay14004@byui.edu>
2018-02-08 20:00:30 -0700

    updated query

--- web/dataRetrieve.php
+++ web/dataRetrieve.php
@@ -51,7 +51,7 @@
     <?php
 
       if(isset($_GET['allEntries'])){
-        $dbconn = pg_connect("host='127.0.0.1' port=5432 dbname=postgres user=postgres password=postgres");
+        $dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres");
         if(! $dbconn){
           $error = error_get_last();
           echo "Error!" . $error['message'];
