dcbc9bed180740a135dbfdb216db6c2c532dbfaf
sydpalmer <hay14004@byui.edu>
2018-02-24 16:00:59 -0700

    fixed minor issue

--- web/update.php
+++ web/update.php
@@ -52,7 +52,7 @@
             $prod = 'earring';
           }
 
-          echo '<option name="id" value="' . $row[0] . '">' . $row[0] . ': Driver - ' . $row[1] . ', Van - ' . $row[2] . ', Date - ' . $row[3] . ', Product: ' . $prod . '</option>';
+          echo '<option value="$row[0]">$row[0]: Driver - $row[1], Van - $row[2], Date - $row[3], Product: $prod . </option>';
         }
         echo "</select>";
       ?>
@@ -80,7 +80,7 @@
 <?php  
 	//Get all the data from the web form
   	$option = $_GET['option'];
-  	$record = $_GET['id'];
+  	$record = $_GET['record'];
   	$input = $_GET['input'];
 
 echo "<p>Option: $option, Record/ID: $record, Input: $input</p>";
