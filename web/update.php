209faaa8d1919ac266b0bb09062c935163a98bf9
sydpalmer <hay14004@byui.edu>
2018-02-24 10:15:03 -0700

    added options

--- web/update.php
+++ web/update.php
@@ -1,3 +1,24 @@
+<?php
+	session_start();
+	// default Heroku Postgres configuration URL
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbopts = parse_url($dbUrl);
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+
+    try {
+    	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    }
+    catch (PDOException $ex) {
+    	print "<p>error: $ex->getMessage() </p>\n\n";
+        die();
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +28,24 @@
 <body>
 
 <h1>Update info</h1>
+<div>
+	<select name="option">
+      <?php
+      	foreach ($db->query("SELECT * FROM shipping") as $row)
+        {
+          if ($row[4] == '1'){
+            $prod = 'bracelet';
+          } else if ($row[4] == '2'){
+            $prod = 'necklace';
+          } else if ($row[4] == '3'){
+            $prod = 'earring';
+          }
 
+          echo "<option>$row[0]: Driver - $row[1], Van - $row[2], Date - $row[3], Product: $prod</option>";
+        }
+      ?>
+    </select>
+</div>
 <?php  ?>
 
 </body>
