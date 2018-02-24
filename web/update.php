014a1120d3de65a18cad429ba15bc18371d232ab
sydpalmer <hay14004@byui.edu>
2018-02-24 16:01:59 -0700

    fixed minor issue

--- web/update.php
+++ web/update.php
@@ -52,7 +52,7 @@
             $prod = 'earring';
           }
 
-          echo '<option value="$row[0]">$row[0]: Driver - $row[1], Van - $row[2], Date - $row[3], Product: $prod . </option>';
+          echo "<option value='$row[0]''>$row[0]: Driver - $row[1], Van - $row[2], Date - $row[3], Product: $prod . </option>";
         }
         echo "</select>";
       ?>
