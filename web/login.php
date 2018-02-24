a2424cdfd1db96e2e84944065f3a44069cb8e800
sydpalmer <hay14004@byui.edu>
2018-02-24 08:57:45 -0700

    login

--- web/login.php
+++ web/login.php
@@ -16,6 +16,25 @@
 <?php
 	session_start();
 
+	// default Heroku Postgres configuration URL
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbopts = parse_url($dbUrl);
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+
+    try {
+    	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    }
+    catch (PDOException $ex) {
+    	print "<p>error: $ex->getMessage() </p>\n\n";
+        die();
+    }
+
 ?>
 
 
