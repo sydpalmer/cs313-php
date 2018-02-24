e9827962d961e59fda128eafcac6393f540718f1
sydpalmer <hay14004@byui.edu>
2018-02-24 08:56:46 -0700

    login

--- web/login.php
+++ web/login.php
@@ -8,12 +8,15 @@
 
 <h1>Sign-In</h1>
 <form method="post">
-   <input type="text" name="username" id="username" placeholder="Username">
-   <input type="password" name="password" id="password" placeholder="Password">
+   <input type="text" name="username" id="username" placeholder="Username"><br>
+   <input type="password" name="password" id="password" placeholder="Password"><br>
    <button type="submit" name="login">Submit</button>
 </form>
 
+<?php
+	session_start();
 
+?>
 
 
 </body>
