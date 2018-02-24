f4712a194ebf7f3f45cf6fcab95010c35ea47f45
sydpalmer <hay14004@byui.edu>
2018-02-24 09:01:51 -0700

    update

--- web/update.php
+++ web/update.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Log in</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+</head>
+<body>
+
+<h1>Sign-In</h1>
+
 <?php  ?>
+
+</body>
+</html>
