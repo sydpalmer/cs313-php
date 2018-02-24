626e841efd8c186c0af528ea80711a5f45703c9c
sydpalmer <hay14004@byui.edu>
2018-02-24 09:00:00 -0700

    login

--- web/login.php
+++ web/login.php
@@ -41,6 +41,34 @@
 
 		$password = $_POST['password'];
 
+		try
+		{
+			$query = "SELECT password, user_id FROM users WHERE username = $username";
+   			$statement = $db->query($query);
+   
+   			$row = $statement->fetch(PDO::FETCH_ASSOC);
+
+   			$valid = password_verify($password, $row['password']);
+
+   			$user_id = $row['user_id'];
+
+   			if ($valid) {
+      			$_SESSION['user_id'] = $user_id;
+      			header("Location: update.php");
+   			} else {
+      			header("Location: login.php");
+   			}
+	
+		}
+		catch (Exception $ex)
+		{
+			// Please be aware that you don't want to output the Exception message in
+			// a production environment
+			echo "Error with DB. Details: $ex";
+			die();
+		}
+
+
 	}
 
 ?>
