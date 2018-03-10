2039268fcd197501376b3e7c84da5860374f284e
sydpalmer <hay14004@byui.edu>
2018-03-10 09:53:58 -0700

    assign9

--- web/assign9/index.js
+++ web/assign9/index.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/math', function(request, response) {
+app.get('/getRate', function(request, response) {
 	handleMath(request, response);
 });
 
