7ce801e1c4ffef582879a7401f513b84aa21312e
sydpalmer <hay14004@byui.edu>
2018-03-10 10:11:20 -0700

    assign9

--- web/assign9/index.js
+++ web/assign9/index.js
@@ -4,10 +4,10 @@ var url = require('url');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/assign9/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/assign9/views');
 app.set('view engine', 'ejs');
 
 app.get('/getRate', function(request, response) {
